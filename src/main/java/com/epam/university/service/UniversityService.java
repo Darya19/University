0689546a99d2d7dfa41472b0ae5f29a237cd13b7
Darya Shcherbina <Darya_Shcherbina@mail.ru>
2020-03-30 11:36:46 +0300

    start using interface for service, logs path changed to target

--- src/main/java/com/epam/university/service/UniversityService.java
+++ src/main/java/com/epam/university/service/UniversityService.java
@@ -1,76 +1,18 @@
 package com.epam.university.service;
 
-import com.epam.university.dao.MarkRepository;
-import com.epam.university.dao.StudentRepository;
-import com.epam.university.domain.Mark;
 import com.epam.university.domain.Student;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+public interface UniversityService {
 
-@Service
-public class UniversityService {
+    int getAverageMarkByStudentId(Integer studentId);
 
-    private Logger logger = LoggerFactory.getLogger(getClass());
+    int getAverageMarkBySubjectId(Integer subjectId);
 
-    private StudentRepository studentRepository;
-    private MarkRepository markRepository;
+    int getAverageMarkBySubjectNGroup(Integer groupId, Integer subjectId);
 
-    public UniversityService(StudentRepository studentRepository, MarkRepository markRepository) {
-        this.studentRepository = studentRepository;
-        this.markRepository = markRepository;
-    }
+    void save(int id, int groupId, String name, String lastName);
 
-    public int getAverageMarkByStudentId(Integer studentId) {
-        logger.info("Get average mark for student '{}'", studentId);
+    Student getById(int id);
 
-        List<Mark> listOfMarks = markRepository.findAllByStudentId(studentId);
-        long sum = listOfMarks.stream().mapToInt(Mark::getMarkId).sum();
-
-        return Math.toIntExact(sum) / listOfMarks.size();
-    }
-
-    public int getAverageMarkBySubjectId(Integer subjectId) {
-        logger.info("Get average mark for university '{}'", subjectId);
-        List<Mark> listOfMarks = markRepository.findAllBySubjectId(subjectId);
-        long sum = listOfMarks.stream().mapToInt(Mark::getMarkId).sum();
-
-        return Math.toIntExact(sum) / listOfMarks.size();
-    }
-
-    public int getAverageMarkBySubjectNGroup(Integer groupId, Integer subjectId) {
-        logger.info("Get average mark for group '{}'", subjectId);
-        List<Student> listOfStudents = studentRepository.findAllByGroupId(groupId);
-        List<List<Mark>> listOfMarks = listOfStudents.stream()
-                .map(Student::getStudentId)
-                .map(markRepository::findAllByStudentId)
-                .collect(Collectors.toList());
-
-        IntStream intStream = listOfMarks.stream().flatMap(List::stream).mapToInt(Mark::getMarkId);
-        long sum = intStream.sum();
-        long count = intStream.count();
-
-        return Math.toIntExact(sum) / Math.toIntExact(count);
-    }
-
-    public void save(int id, int groupId, String name, String lastName) {
-        Student s = new Student();
-        s.setStudentId(id);
-        s.setGroupId(groupId);
-        s.setFirstName(name);
-        s.setLastName(lastName);
-        studentRepository.save(s);
-    }
-
-    public Student getById(int id) {
-        return studentRepository.findById(id).get();
-    }
-
-    public Iterable<Student> findAll() {
-        return studentRepository.findAll();
-    }
+    Iterable<Student> findAll();
 }
